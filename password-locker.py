bfb29352b0948c478b3cc4412523b44c80b3d0ce
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-19 15:04:46 +0300

    create a user class to store and define each instance of a user

---
+++ password-locker.py
@@ -0,0 +1,13 @@
+class User:
+    '''
+    Create a user class to generate new instances of a User
+    '''
+
+    User = []
+
+    def __init__(self, userName, passWord):
+        '''
+        add a method to define the properties of a user
+        '''
+        self.userName = userName
+        self.passWord = passWord
