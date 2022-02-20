5258e5cca843fd92f9a3106f2e0123f0b86f1012
jerushaotieno <jerushaotienocoding@gmail.com>
2022-02-20 18:49:00 +0300

    add functions for user to interact with account credentials side of application

--- interface.py
+++ interface.py
@@ -30,3 +30,42 @@ def userLogin(userName, passWord):
 
 #credential-side functions
 
+def createNewCredential(accountName,userName,passWord):
+    '''
+    Function creates new credentials for a user's account
+    '''
+    newCredential = Credentials(accountName, userName, passWord)
+    return newCredential
+
+def saveCredentials(Credentials):
+    '''
+    Function saves user credentials to the credentials directory
+    '''
+    Credentials. saveCredentials()
+
+def displayCredentials():
+    '''
+    Function returns saved user credentials
+    '''
+    return Credentials.displayCredentials()
+
+def deleteCredentials(Credentials):
+    '''
+    Function deletes existing user credentials from the credentials directory
+    '''
+    Credentials.deleteCredentials()
+
+def searchCredentials(accountName):
+    '''
+    Function finds user credentials using given account name and returns the specific account
+    '''
+    return Credentials.searchCredentials(accountName)
+
+def credendtialsExist(accountName):
+    '''
+    Function checks if user credentials exist and returns a True or False value
+    '''
+    return Credentials.credentialsExist(accountName)
+
+
+
